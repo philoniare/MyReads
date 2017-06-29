0e0a70b0fb11f222cb55658035ca5a7a4a64f0cc
philoniare <philoniare@gmail.com>
2017-06-29 14:51:06 -0700

    Reformatted code to conform to Google JS style guide

--- src/App.js
+++ src/App.js
@@ -1,77 +1,79 @@
 import React, {Component} from 'react';
-import {Route} from 'react-router-dom'
-import { createMuiTheme, MuiThemeProvider } from 'material-ui/styles';
+import {Route} from 'react-router-dom';
+import {createMuiTheme, MuiThemeProvider} from 'material-ui/styles';
 import {blue, red, green} from 'material-ui/styles/colors';
 import createPalette from 'material-ui/styles/palette';
-import BookShelf from './components/BookShelf'
-import BookSearchView from './components/BookSearchView'
-import AppBar from './components/AppBar'
-import * as BooksAPI from './utils/BooksAPI'
+import BookShelf from './components/BookShelf';
+import BookSearchView from './components/BookSearchView';
+import AppBar from './components/AppBar';
+import * as BooksAPI from './utils/BooksAPI';
 import './App.css';
 
-
 const myReadsTheme = createMuiTheme({
-    palette: createPalette({
-        primary: blue,
-        accent: {
-            ...green,
-            A400: '#00e677',
-        },
-        error: red,
-    }),
+  palette: createPalette({
+    primary: blue,
+    accent: {
+      ...green,
+      A400: '#00e677',
+    },
+    error: red,
+  }),
 });
 
 class App extends Component {
-    fetchAndDisplayShelves = () => {
-        BooksAPI.getAll().then(books => {
-            const shelves = {
-                currentlyReading: [],
-                wantToRead: [],
-                read: []
-            }
-            books.forEach(book => {
-                shelves[book.shelf].push(book)
-            })
-            this.setState({shelves})
-        })
-    }
-    componentDidMount() {
-        this.fetchAndDisplayShelves()
-    }
-    state = {
-        shelves: {
-            currentlyReading: [],
-            wantToRead: [],
-            read: [],
-        },
-    }
-    render() {
-        const baseUrl = process.env.PUBLIC_URL;
-        const {currentlyReading, wantToRead, read} = this.state.shelves
-        return (
-            <MuiThemeProvider theme={myReadsTheme}>
-                <div className="App">
-                    <Route exact path={baseUrl + "/"} render={() => (
-                        <div>
-                            <AppBar color="primary" />
-                            <BookShelf
-                                onShelfChange={this.fetchAndDisplayShelves}
-                                category="currentlyReading" books={currentlyReading} />
-                            <BookShelf
-                                onShelfChange={this.fetchAndDisplayShelves}
-                                category="wantToRead" books={wantToRead} />
-                            <BookShelf
-                                onShelfChange={this.fetchAndDisplayShelves}
-                                category="read" books={read} />
-                        </div>
-                    )}/>
-                    <Route path={baseUrl + "/search"} render={() => (
-                        <BookSearchView onShelfChange={this.fetchAndDisplayShelves} />
-                    )}/>
+  fetchAndDisplayShelves = () => {
+    BooksAPI.getAll().then(books => {
+      const shelves = {
+        currentlyReading: [],
+        wantToRead: [],
+        read: [],
+      };
+      books.forEach(book => {
+        shelves[book.shelf].push(book);
+      });
+      this.setState({shelves});
+    });
+  };
+
+  componentDidMount() {
+    this.fetchAndDisplayShelves();
+  }
+
+  state = {
+    shelves: {
+      currentlyReading: [],
+      wantToRead: [],
+      read: [],
+    },
+  };
+
+  render() {
+    const baseUrl = process.env.PUBLIC_URL;
+    const {currentlyReading, wantToRead, read} = this.state.shelves;
+    return (
+        <MuiThemeProvider theme={myReadsTheme}>
+          <div className="App">
+            <Route exact path={baseUrl + '/'} render={() => (
+                <div>
+                  <AppBar color="primary"/>
+                  <BookShelf
+                      onShelfChange={this.fetchAndDisplayShelves}
+                      category="currentlyReading" books={currentlyReading}/>
+                  <BookShelf
+                      onShelfChange={this.fetchAndDisplayShelves}
+                      category="wantToRead" books={wantToRead}/>
+                  <BookShelf
+                      onShelfChange={this.fetchAndDisplayShelves}
+                      category="read" books={read}/>
                 </div>
-            </MuiThemeProvider>
-        );
-    }
+            )}/>
+            <Route path={baseUrl + '/search'} render={() => (
+                <BookSearchView onShelfChange={this.fetchAndDisplayShelves}/>
+            )}/>
+          </div>
+        </MuiThemeProvider>
+    );
+  }
 }
 
 export default App;
--- src/components/BookSearchView.js
+++ src/components/BookSearchView.js
@@ -1,59 +1,61 @@
-import React, {Component} from 'react'
+import React, {Component} from 'react';
 import {Link} from 'react-router-dom';
 import AppBar from 'material-ui/AppBar';
 import Toolbar from 'material-ui/Toolbar';
 import IconButton from 'material-ui/IconButton';
 import ArrowBack from 'material-ui-icons/ArrowBack';
-import TextField from 'material-ui/TextField'
-import BookShelf from './BookShelf'
-import * as BooksAPI from '../utils/BooksAPI'
-
+import TextField from 'material-ui/TextField';
+import BookShelf from './BookShelf';
+import * as BooksAPI from '../utils/BooksAPI';
 
 class BookSearchView extends Component {
-    constructor(props) {
-        super(props)
-        this.queryBooks = this.queryBooks.bind(this)
+  constructor(props) {
+    super(props);
+    this.queryBooks = this.queryBooks.bind(this);
+  }
+
+  state = {
+    query: '',
+    books: [],
+  };
+
+  queryBooks(event) {
+    const query = event.target.value;
+    if (query) {
+      BooksAPI.search(query, 10).then(books => {
+        if (books.error) this.setState({books: []});
+        else this.setState({books});
+      });
+    } else {
+      this.setState({books: []});
     }
-    state = {
-        query: '',
-        books: [],
-    }
-    queryBooks(event) {
-        const query = event.target.value
-        if(query) {
-            BooksAPI.search(query, 10).then(books => {
-                if(books.error) this.setState({books: []})
-                else this.setState({books})
-            })
-        } else {
-            this.setState({books: []})
-        }
-        this.setState({query})
-    }
-    render() {
-        return (
-            <div className="app">
-                <AppBar position="static" color="primary">
-                    <Toolbar>
-                        <Link to="/">
-                            <IconButton color="contrast" aria-label="Back">
-                                <ArrowBack />
-                            </IconButton>
-                        </Link>
-                        <TextField
-                            placeholder="Search Query"
-                            value={this.state.query}
-                            onChange={this.queryBooks}
-                        />
-                    </Toolbar>
-                </AppBar>
-                <BookShelf category="search"
-                           onShelfChange={this.props.onShelfChange}
-                           books={this.state.books}
-                />
-            </div>
-        )
-    }
+    this.setState({query});
+  }
+
+  render() {
+    return (
+        <div className="app">
+          <AppBar position="static" color="primary">
+            <Toolbar>
+              <Link to="/">
+                <IconButton color="contrast" aria-label="Back">
+                  <ArrowBack />
+                </IconButton>
+              </Link>
+              <TextField
+                  placeholder="Search Query"
+                  value={this.state.query}
+                  onChange={this.queryBooks}
+              />
+            </Toolbar>
+          </AppBar>
+          <BookShelf category="search"
+                     onShelfChange={this.props.onShelfChange}
+                     books={this.state.books}
+          />
+        </div>
+    );
+  }
 }
 
-export default BookSearchView
+export default BookSearchView;
